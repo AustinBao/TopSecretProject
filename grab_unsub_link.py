998935edbe60b2a6f931c6797154914dbc3d8ee6
githubconoisseur <galileokim451@gmail.com>
2023-08-27 12:16:33 -0600

    added a grab unsubscribe link file
    Made this change to help with modularization.

--- grab_unsub_link.py
+++ grab_unsub_link.py
@@ -8,7 +8,7 @@ imap_url = 'imap.gmail.com'
 def search(key, value, con):
 	result, data = con.search(None, key, '"{}"'.format(value))
 	return data
-
+#not modularizing yet
 def get_emails(result_bytes):
 			# [b'1 2 3 4 5 6']  
 	for num in result_bytes[0].split():
@@ -16,12 +16,6 @@ def get_emails(result_bytes):
 
 		message = email.message_from_bytes(data[0][1])
 		main_body = message.walk()
-
-
-		print(f"From: {message.get('From')}")
-		print(f"To: {message.get('To')}")
-		print(f"Date: {message.get('Date')}")
-		print(f"Subject: {message.get('Subject')}")
 
 		for part in main_body:
 			if part.get_content_type() == "text/plain":
@@ -51,9 +45,6 @@ def get_emails(result_bytes):
 						final = final.removeprefix("<")
 						final = final.removesuffix(">")
 
-						
-		print("Link: " + final)
-
 con = imaplib.IMAP4_SSL(imap_url)
 
 con.login(user, password)
@@ -64,9 +55,3 @@ con.select('Inbox')
 get_emails(search('FROM', 'galileokim451@gmail.com', con))
 
 
-
-# 0. Learn object oriented programming for python 
-# 1. make functions less that or equal to 7 lines (aka does only one function)
-# 2. make a unit test
-# 3. Learn about refactoring code: https://code.visualstudio.com/docs/editor/refactoring#:~:text=Refactoring%20actions,-Extract%20Method&text=Select%20the%20source%20code%20you,function%20at%20various%20different%20scopes.
-# 4. Modularize
