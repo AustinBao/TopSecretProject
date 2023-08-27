a7bb69356fae7e67a7f4b0858365ef45b384daf1
Austin Bao <austin_dun@outlook.com>
2023-08-26 21:59:09 -0600

    got link success

--- testing.py
+++ testing.py
@@ -19,17 +19,45 @@ def get_emails(result_bytes):
 		typ, data = con.fetch(num, '(RFC822)')
 
 		message = email.message_from_bytes(data[0][1])
+		print(type(message))
+		main_body = message.walk()
+
 
 		print(f"From: {message.get('From')}")
 		print(f"To: {message.get('To')}")
 		print(f"Date: {message.get('Date')}")
 		print(f"Subject: {message.get('Subject')}")
 
-		print("Content: ")
-		for part in message.walk():
+		for part in main_body:
 			if part.get_content_type() == "text/plain":
-				print(part.as_string())
+				
+				chunk = part.as_string()
+				indi = chunk.split()
+				# print(indi)
 
+				for words in indi:
+					if words == "Unsubscribe":
+						curr_word_index = int(indi.index(words))
+						link_index = int(curr_word_index) + 1
+						index_counter = 1
+
+						if ">" not in indi[link_index]:
+							indi[link_index] = indi[link_index].removesuffix("=")
+							for link_section in indi[link_index + index_counter:]:
+								indi[link_index + index_counter] = indi[link_index + index_counter].removesuffix("=")
+								index_counter += 1
+								if ">" in link_section:
+									break
+
+						# print(indi[link_index:link_index+index_counter])
+						
+						final = ""
+						final = final.join(indi[link_index:link_index+index_counter])
+						final = final.removeprefix("<")
+						final = final.removesuffix(">")
+
+						
+		print("Link: " + final)
 
 con = imaplib.IMAP4_SSL(imap_url)
 
