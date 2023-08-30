a5b1e0869817cfa725ab262d103d416291865e70
Austin Bao <austin_dun@outlook.com>
2023-08-30 16:47:52 -0600

    turn data into txt file

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,11 +1,18 @@
 
 button = document.getElementById("signupbtn")
-button.addEventListener('click', confirm)
+button.addEventListener('click', write_user_info_to_txt)
 
-function confirm() {
+function write_user_info_to_txt() {
+    const fs = require("fs");
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    console.log(email,password)
+    const user_info = [email, password];
+
+    fs.writeFile("./test.txt", email, (err) => {
+        if (err) {
+            console.error(err);
+        return;
+          }
+        });
+        console.log("Data has been Written");
 }
-
-
