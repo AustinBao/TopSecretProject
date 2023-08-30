d39f85e022850c1003d1f13742733fe33c7387e8
githubconoisseur <galileokim451@gmail.com>
2023-08-30 15:50:55 -0600

    made signup button work

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,18 +1,11 @@
 
-// document.getElementById("signupbtn").addEventListener('click', confirm);
-// document.addEventListener('mousedown', hello)
-
-button = document.getElementById("btn")
+button = document.getElementById("signupbtn")
 button.addEventListener('click', confirm)
+
 function confirm() {
-    alert("hello")
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    console.log(email,password)
 }
-// function confirm() {
-//     var email = document.getElementById("email");
-//     var password = document.getElementById("password");
-//     console.log("working")
-//     console.log("Username: " + email);
-//     console.log("Password: " + password);
-// }
 
 
