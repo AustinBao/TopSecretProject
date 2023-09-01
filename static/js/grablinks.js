f89d26457a0ae3c4b28d594f9b0f6c9e2c3820e7
Austin Bao <austin_dun@outlook.com>
2023-08-31 19:25:08 -0600

    autocomplete search bar

--- static/js/grablinks.js
+++
@@ -1,9 +0,0 @@
-function searched(){
-    const searchform = document.getElementById('searchform');
-    const searchbar = document.getElementById('searchbar').value;
-    
-    console.log(searchform, searchbar)
-}
-
-
-searchform.addEventListener('submit', searched);
