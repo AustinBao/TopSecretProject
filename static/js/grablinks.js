417f546b40a11311ef6024ca4a843d8bc5471dd1
Austin Bao <austin_dun@outlook.com>
2023-09-03 12:48:44 -0600

    corrected grablinks

--- static/js/grablinks.js
+++ static/js/grablinks.js
@@ -1,4 +1,7 @@
+const currentUrl = window.location.href;
+console.log(currentUrl);
 
-fetch("http://localhost:5000/grablinks/26ef0160193549f781c06f483358e1d7")
-  .then(res => res.json())
-  .then(data => console.log(data)) 
+
+// fetch("http://localhost:5000/grablinks/")
+//   .then(res => res.json())
+//   .then(data => console.log(data)) 
